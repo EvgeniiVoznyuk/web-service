06858af70c3f64351a9f574e0d69c07ae627beb4
Evgenii <voznukzhenia@gmail.com>
2021-05-22 14:04:25 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,46 @@
 <template>
   <div id="app">
     <b-navbar type="dark" variant="dark">
-    <b-navbar-nav>
-      <b-nav-item href="#"></b-nav-item>
-       <b-nav-item href="#">Система моніторингу стану</b-nav-item>
-    </b-navbar-nav>
-  </b-navbar>
-  <div class="container mt-3">
-    <b-card
-      title=""
-      tag="article"
-      style="width: 500px; height: 600px;"
-      class="mb-2"
-    >
-    </b-card>
-    <b-card
-      title=""
-      tag="article"
-      style="width: 500px; height: 600px;"
-      class="mb-2"
-    >
-    </b-card>
-    <b-card
-      title=""
-      tag="article"
-      style="width: 500px; height: 600px;"
-      class="mb-2"
-    >
-    </b-card>
-  </div>
+      <b-navbar-nav>
+        <b-nav-item href="#"></b-nav-item>
+        <b-nav-item href="#">Система моніторингу стану</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <div class="container mt-3">
+      <b-card
+        title=""
+        tag="article"
+        style="width: 600px; height: 400px"
+        class="mb-2"
+      >
+        <div class="content">
+          <img class="img" src="~@/assets/img/1.png" alt="" />
+          <b-card-text> Температура: 18 C </b-card-text>
+        </div>
+      </b-card>
+      <b-card
+        title=""
+        tag="article"
+        style="width: 600px; height: 400px"
+        class="mb-2"
+      >
+         <div class="content">
+          <img class="img" src="~@/assets/img/2.png" alt="" />
+          <b-card-text> Вологісті: 67 % </b-card-text>
+        </div>
+      </b-card>
+      <b-card
+        title=""
+        tag="article"
+        style="width: 600px; height: 400px"
+        class="mb-2"
+      >
+         <div class="content">
+          <img class="img" src="~@/assets/img/3.png" alt="" />
+          <b-card-text> Освітленість: 150 Люкс </b-card-text>
+        </div>
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -43,6 +55,19 @@ export default {
 .container {
   padding-top: 150px;
   display: flex;
-  gap: 60px;
+  gap: 80px;
+}
+.img {
+  width: 300px;
+}
+.content {
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  font-size: 20px;
+  font-weight: bold;
 }
 </style>
